9791db4c1596ab25ea4339cedf3344b36f27cd14
Joshua Byrd <phocks@gmail.com>
2019-06-03 15:51:08 +1000

    Adding fitBounds as animation

--- src/index.js
+++ src/index.js
@@ -6,8 +6,10 @@ import styles from "./styles.scss";
 let map; // Mapbox
 
 const Mapboxer = props => {
+  // Setup element reference for Mapbox to attach to
   const inputEl = useRef(null);
 
+  // Set up state
   const [initialised, setInitialised] = useState(false);
 
   // On mount initialisation code
@@ -16,9 +18,9 @@ const Mapboxer = props => {
 
     map = new mapboxgl.Map({
       container: inputEl.current,
-      attributionControl: false,
+      attributionControl: props.attributionControl,
       style: props.setStyle,
-      interactive: false, // Stops mouse stopping animation
+      interactive: props.interactive, // Stops mouse stopping animation
       bounds: props.setBounds // Australia [[103.4,-47.7],[163.0,-2.7]]
     });
 
@@ -37,12 +39,20 @@ const Mapboxer = props => {
     map.fitBounds(props.setBounds, {animate: false});
   }, [props.setBounds])
 
+  // Fit to bounds with animation
+  useEffect(() => {
+    if (!initialised) return;
+    map.fitBounds(props.fitBounds);
+  }, [props.fitBounds])
+
   return <div className={styles.root} ref={inputEl} />;
 };
 
 Mapboxer.defaultProps = {
   setStyle: "mapbox://styles/mapbox/light-v10",
-  setBounds: [[103.4,-47.7],[163.0,-2.7]]
+  setBounds: [[103.4,-47.7],[163.0,-2.7]],
+  attributionControl: {},
+  interactive: false
 }
 
 export default Mapboxer;
